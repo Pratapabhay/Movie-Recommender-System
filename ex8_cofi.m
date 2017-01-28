2ce0721bbcaa6552b78bdf9819af58fe3135396c
Pratapabhay <pratapabhay981@gmail.com>
2017-01-28 23:40:01 +0530

    Ratings Updated Commit

--- ex8_cofi.m
+++ ex8_cofi.m
@@ -136,14 +136,7 @@ my_ratings(98) = 2;
 % We have selected a few movies we liked / did not like and the ratings we
 % gave are as follows:
 my_ratings(7) = 3;
-my_ratings(12)= 5;
-my_ratings(54) = 4;
-my_ratings(64)= 5;
-my_ratings(66)= 3;
-my_ratings(69) = 5;
-my_ratings(183) = 4;
-my_ratings(226) = 5;
-my_ratings(355)= 5;
+my_ratings(12)= 4;
 
 fprintf('\n\nNew user ratings:\n');
 for i = 1:length(my_ratings)
